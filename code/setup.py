974526c0705a55635c2cf96d247302755992c939
Kai Dietrich <mail@cleeus.de>
2009-06-05 18:31:26 +0200

    Prepare for distributions

--- code/setup.py
+++ code/setup.py
@@ -3,10 +3,9 @@ from distutils.core import setup
 
 setup(name='wiki2beamer',
     version='0.7alpha2',
-    py_modules=['wiki2beamer'],
-    scripts=['wiki2beamer.py'],
+    scripts=['wiki2beamer'],
     description='Create LaTeX-beamer presentations with a wiki syntax',
     author='Kai Dietrich, Michael Rentzsch',
-    author_email='mail@cleeus.de',
+    author_email='mail@cleeus.de, michael.rentzsch@repc.de',
     url='http://wiki2beamer.sourceforge.net/'
 )
