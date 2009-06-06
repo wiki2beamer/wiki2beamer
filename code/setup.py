ffa0021e6cc8e9905c9d9ce991ea0b8b65de81de
Kai Dietrich <mail@cleeus.de>
2009-06-06 09:28:55 +0200

    final minor changes for 0.7 release

--- code/setup.py
+++ code/setup.py
@@ -5,10 +5,10 @@
 from distutils.core import setup
 
 setup(name='wiki2beamer',
-    version='0.7alpha2',
+    version='0.7',
     scripts=['wiki2beamer'],
     description='Create LaTeX-beamer presentations with a wiki syntax',
-    author='Kai Dietrich, Michael Rentzsch',
-    author_email='mail@cleeus.de, michael.rentzsch@repc.de',
+    author='Michael Rentzsch, Kai Dietrich',
+    author_email='mmichael.rentzsch@repc.de, mail@cleeus.de',
     url='http://wiki2beamer.sourceforge.net/'
 )
