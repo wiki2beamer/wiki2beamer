c2d9545ced2c8a1b914f781a489821a833c521c2
Kai Dietrich <mail@cleeus.de>
2009-10-16 19:59:34 +0200

    version number updated

--- code/setup.py
+++ code/setup.py
@@ -17,7 +17,7 @@
 from distutils.core import setup
 
 setup(name='wiki2beamer',
-    version='0.8',
+    version='0.81',
     scripts=['wiki2beamer'],
     description='Create LaTeX-beamer presentations with a wiki syntax',
     author='Michael Rentzsch, Kai Dietrich',
