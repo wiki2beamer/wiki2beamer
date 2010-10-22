c3525f80cb61776e4343dba265456d97b71e00fb
Valentin Haenel <valentin.haenel@gmx.de>
2010-10-22 17:57:46 +0200

    feature: file inclusion
    Syntax should be >>>filename<<<, and w2b should replace this with the lines from
    the respective file.
    
    This is the test for such a function.

--- tests/test_wiki2beamer.py
+++ tests/test_wiki2beamer.py
@@ -239,6 +239,12 @@ class TestConvert2Beamer(unittest.TestCase):
         out = convert2beamer(lines)
         self.assertEqual(out,expected)
 
+    def test_include_file(self):
+        expected = ['test file content']
+        line = ">>>test_file<<<"
+        out = include_file(line)
+        self.assertEqual(expected, out)
+
 class TestSelectedFramesMode(unittest.TestCase):
     def setUp(self):
         return
